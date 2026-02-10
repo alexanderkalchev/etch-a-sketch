bc271f907f0f4c48fd76a604c6cf540bbe23749c
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-10 10:33:05 +0200

    Add generate button, reset button, footer

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const container = document.querySelector(".container");
-for(let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.setAttribute("class", "square");
-    container.appendChild(div);
-}
+const generateButton = document.querySelector(".generate");
+const resetButton = document.querySelector(".reset");
 
 container.addEventListener("mouseover", (e) => {
     if(e.target.classList.contains("square")){
         e.target.classList.add("over");
     }
-})
+});
 
-// container.addEventListener("mouseout", (e) => {
-//     if(e.target.classList.contains("square")){
-//         e.target.classList.remove("over");
-//     }
-// })
+generateButton.addEventListener("click", (e) => {
+    let size = prompt("Enter size");
+    console.log(size);
+    generateGrid(size, container);
+});
+
+resetButton.addEventListener("click", (e) => {
+    const divs = document.querySelectorAll("div.square");
+    divs.forEach((node) => node.classList.remove("over"));
+});
+
+function generateGrid(size, container){
+    container.innerHTML = "";
+    const squareSide = (960 - (size - 1)*4)/size;
+    for(let i = 0; i < size**2; i++){
+        const div = document.createElement("div");
+        div.setAttribute("style", `width: ${squareSide}px; height: ${squareSide}px`);
+        div.setAttribute("class", "square");
+        container.appendChild(div);
+    }   
+}
+
+generateGrid(16, container);
+
